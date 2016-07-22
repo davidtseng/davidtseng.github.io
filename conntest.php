800f3834dc5ad1e87498e34e46214ddb1cb27f1b
David Tseng <daviet99@gmail.com>
2016-07-22 18:19:12 +0000

    dbname

--- conntest.php
+++ conntest.php
@@ -2,9 +2,10 @@
 $servername = "us-cdbr-iron-east-04.cleardb.net";
 $username = "b83ae8dff5984c";
 $password = "e484cf1c";
+$dbname = "ad_901f7e03732dc89";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
